6905c651f97cad38d4451a6c33d7334748d2204f
betodasilva <jrs_robert@hotmail.com>
2018-03-31 08:02:11 -0300

    Adiciona estilos e interações para lista

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="list-container">
     <ul class="user-list">
-      <li v-for="(user, index) in activeUsers" :key="index" class="user">
-        <span class="user__first-name">{{ user.name.first }}</span>
-        <span class="user__last-name">{{user.name.last}}</span>
+      <li v-for="(user, index) in users" :key="index" class="user" v-on:click="openUserData($event, index)">
+        <div class="user-info">
+          <span class="user__first-name">{{ user.name.first }}</span>
+          <span class="user__last-name">{{user.name.last}}</span>
+          <span class="user__id small label">id: {{user.id}}</span>  
+        </div>
+        
         <table class="user-data closed">
           <tr>
-            <th>Id</th>
-            <th>Status</th>
-            <th>Avaliações</th>
+            <th>Status do Usuário</th>
+            <th>Notas das Avaliações</th>
           </tr>
           <tr>
-            <td>{{user.id}}</td>
-            <td>{{user.active}}</td>
+            <td v-if="user.active === 1">
+              <span class="label success">Ativo</span>
+            </td>
+            <td v-else>
+              <span class="label error">Inativo</span>
+            </td>
             <td>
-                <span v-for="(rating, ind) in user.rating" :key="ind">
-                  {{ rating }}
+                <span v-for="(rating, ind) in user.rating" :key="ind" class="user__rating label">
+                  #{{ind +1}}
+                  <span class="label badge focus">
+                    {{ rating }}
+                  </span>
                 </span>
             </td>
           </tr>
         </table>
       </li>
     </ul>
+    <form @submit.prevent="addUser">
+      <input type="text" placeholder="Adicione um formulario">
+    </form>
     
   </div>
 </template>
@@ -36,11 +49,18 @@ export default {
       users: Users
     }
   },
-  computed: {
-    activeUsers: function() {
-      return this.users.filter(u => u.active == 1 );
+  methods: {
+    openUserData: function(e, index){
+      const ele = e.target.tagName == 'LI' ? e.target : e.target.parentNode;
+      const tableData = ele.querySelector('.user-data');
+      ele.classList.toggle('open-data');
+      tableData.classList.toggle('open');
+    },
+    addUser() {
+      
     }
-  }
+  },
+
 }
 </script>
 
