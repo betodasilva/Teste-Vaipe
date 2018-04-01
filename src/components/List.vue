dfc634077cc83b530fdbd9659ba2e13e6be99e2b
betodasilva <jrs_robert@hotmail.com>
2018-04-01 14:14:52 -0300

    Adiciona formulário para adição de novos usuários

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="list-container">
     <ul class="user-list">
-      <li v-for="(user, index) in users" :key="index" class="user" v-on:click="openUserData($event, index)">
+       <li class="user add-new" v-on:click="openUserData($event)">
+        <span class="title">Adicionar novo usuário</span>
+        <form @submit.prevent="addUser" class="user-data row gutters">
+          <input type="text" name="first-name" placeholder="Primeiro nome" class="col col-6" v-model="newUser.name.first">
+          <input type="text" name="last-name" placeholder="Sobrenome" class="col col-6" v-model="newUser.name.last">
+          <fieldset class="col col-12">
+            <h5>Avaliações</h5>
+            <div class="row gutters">
+              <div class="col col-4">
+                <label for="rating-1">Primeira Nota</label>
+                <input type="number" name="rating-1" max="10" v-model="newUser.rating[0]">
+              </div>
+              <div class="col col-4">
+                <label for="rating-2">Segunda Nota</label>
+                <input type="number" name="rating-2" max="10" v-model="newUser.rating[1]">
+              </div>
+              <div class="col col-4">
+                <label for="rating-3">Terceira Nota</label>
+                <input type="number" name="rating-3" max="10" v-model="newUser.rating[2]">
+              </div>
+            </div>
+          </fieldset>
+          <div class="col col-12">
+            <button type="submit" class="button big">Adicionar</button>
+          </div>
+        
+        </form>
+      </li>
+      <li v-for="(user, index) in users" :key="index" class="user" v-on:click="openUserData($event)">
         <div class="user-info">
           <span class="user__first-name">{{ user.name.first }}</span>
           <span class="user__last-name">{{user.name.last}}</span>
@@ -31,10 +59,9 @@
           </tr>
         </table>
       </li>
+
     </ul>
-    <form @submit.prevent="addUser">
-      <input type="text" placeholder="Adicione um formulario">
-    </form>
+    
     
   </div>
 </template>
@@ -46,18 +73,31 @@ export default {
   name: 'List',
   data(){
     return {
-      users: Users
+      users: Users,
+      newUser: {
+        id: Number,
+        name: {
+          first: '',
+          last: ''
+        },
+        rating: []
+      }
     }
   },
   methods: {
-    openUserData: function(e, index){
+    openUserData: function(e){
       const ele = e.target.tagName == 'LI' ? e.target : e.target.parentNode;
-      const tableData = ele.querySelector('.user-data');
-      ele.classList.toggle('open-data');
-      tableData.classList.toggle('open');
+      if (ele.tagName == 'LI' ) {
+        const tableData = ele.querySelector('.user-data');
+        ele.classList.toggle('open-data');
+        tableData.classList.toggle('open');
+      }
+      
     },
     addUser() {
-      
+      const newID = this.users[this.users.length -1].id + 1;
+      this.newUser.id = newID;
+      this.users.push(this.newUser);
     }
   },
 
